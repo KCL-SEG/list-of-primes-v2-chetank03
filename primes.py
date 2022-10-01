c7eec13e0fb68c110fe99d21ab3debc54206566b
Chetan Krishna Kodeboyina <chetan.kodeboyina@kcl.ac.uk>
2022-10-01 17:55:45 +0100

    Add to raise the exceptions if required
    checks if number entered is positve raise exception if not.

--- primes.py
+++ primes.py
@@ -2,5 +2,25 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+
+    if number_of_primes < 1:
+        raise ValueError(f"number of primes = {number_of_primes} should have beem a positive number.") # Raises a value error if number is not positive.
+
+
+        
     list = []
+    count = 0 #Keeps the count of for the numbers needed to be print.
+    num = 2 # Start with smallest prime and get incremented.
+    while count < number_of_primes:  # Keeps running till entered number of prime numbers are added to the list.
+       
+        for i in range(2,(num//2)+1):  # Loops from 2 to (num//2)+1, to check the if the number is divisibility
+            if num%i==0: 
+                break 
+        else:               #If number is not divivsible, num is added to the list
+            list.append(num);
+            count+=1  #increments the count
+        num+=1  #increments the number
+
     return list
+
+
